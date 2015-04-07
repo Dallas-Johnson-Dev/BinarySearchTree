262c517215f932a9c11d34c2a743d419690df40c
silverbullets <djohnson1208@yahoo.com>
2015-04-07 01:18:50 -0400

    Added some other code to the classes since apparently it wasn't written right the first time... :s

--- SearchTree.py
+++ SearchTree.py
@@ -5,48 +5,46 @@
 #Tree class: this is the top-level binary tree. This will only really store the root as a variable, and will house the search and modify methods.
 
 class tree:
-    root = None
 
-    def __init__(self, rootNode = 0): #assuming that we arent' going to put in a node, but we totally can.
-        root = rootNode
+    def __init__(self, rootNode = None): #assuming that we arent' going to put in a node, but we totally can.
+        self.root = rootNode
 
     def add(self, newNode): #Allows us to add a node to the tree, we may not be allowed to do this in program 4, but since a tree should know how to add nodes fuck it, we'll include this.
         if not root:
-            root = newNode
+            self.root = newNode
         else:
-            root.addNode(newNode)
+            self.root.addNode(newNode)
 
 #Node class: this is what holds our data. The class consists of the left node, right node, parent node, and the data. We'll also set a boolean flag for whether or not we're root.  
 
 class node:
-    parent = data = left = right = None #That initialization chain tho.
-    count = 0 #If the two strings we're comparing are equal then we just add to this counter here.
-    isRoot = False
 
     def __init__(self, newParent = None, newData = None, newLeft = None, newRight = None):
-        parent = newParent
-        data = newData
-        left = newLeft
-        right = newRight
+        self.parent = newParent
+        self.data = newData
+        self.left = newLeft
+        self.right = newRight
+        self.count = 0 #This is for if we have multiple versions of the node.
+        self.isRoot = False 
 
         #We'll disable the root flag by default. I'm only doing this because it's never going to be root unless there's no parent set. If there's no parent then it must be part of it's own tree if another Root already exists.
         if not parent:
-            isRoot = True
+            self.isRoot = True
 
     def getRight(self): #returns the node to the left. We probably don't need to do this but I'm including these anyway.
         return right
     
     def setRight(self, newRight): #Allows us to declare a new right node.
-        right = newRight
+        self.right = newRight
 
     def getLeft(self): #returns the node to the right. Again, probably overkill because python but I'm including them to be easily translated into c++ when the time comes.
         return left
 
     def setLeft(self, newLeft): #Allows us to set a new left node.
-        left = newLeft
+        self.left = newLeft
 
     def setData(self, newData): #Allows us to set data.
-        data = newData
+        self.data = newData
 
     def getData(self): #Allows us to return data from a node.
         return data
@@ -57,18 +55,18 @@ class node:
     #instantiated.
     def addNode(self, newNode): #Recursive function for adding a node to the tree
         if newNode.getData() == data:
-            count = count + 1
+            self.count++
             return
         if newNode.getData() < data:
             if not left:
-                left = newNode
+                self.left = newNode
             else:
-                left.addNode(newNode)
+                self.left.addNode(newNode)
         elif newNode.getData() > data:
             if not right:
-                right = newNode
+                self.right = newNode
             else:
-                right.addNode(newNode)
+                self.right.addNode(newNode)
 
     #Deletion Rules made into their own methods for ease of use when deleting.
 
@@ -82,48 +80,48 @@ class node:
             #is a pretty safe way to figure it out imo.
             #Left pointer on the parent
             if parent.getData() > data:
-                temp = self
-                parent.setLeft(left)
+                self.temp = self
+                self.parent.setLeft(left)
                 del temp
             #Right pointer on the parent
             elif parent.getData() < data:
-                temp = self
-                parent.setRight(self)
+                self.temp = self
+                self.parent.setRight(self)
                 del temp
         elif not left:
             #Left pointer on the parent
             if parent.getData() > data:
-                temp = self
-                parent.setLeft(right)
+                self.temp = self
+                self.parent.setLeft(right)
                 del temp
             #Right pointer on the parent
             elif parent.getData() < data:
-                temp = self
-                parent.setRight(right)
+                self.temp = self
+                self.parent.setRight(right)
                 del temp
 
     def doubleChildRule(self):
-        currentNode = self
-        finalLeft = None
+        self.currentNode = self
+        self.finalLeft = None
 
         #Go right once. This is the first step in this method.
-        finalLeft = right
+        self.finalLeft = right
         #We need to iterate the rest of the way down. A loop will suffice for this for simplicity's sake.
-        while finalLeft.getLeft():
-            finalLeft = finalLeft.getLeft()
-        currentNode.setData(finalNode.getData())
+        while self.finalLeft.getLeft():
+            self.finalLeft = finalLeft.getLeft()
+        self.currentNode.setData(finalNode.getData())
         #Now we need to check and see which cases we have here. We shouldn't have any double children again, so now it's either
         #Single or no child rule.
-        if not left and not right:
+        if not self.left and not self.right:
             noChildRule()
-        elif not left or not right:
+        elif not self.left or not self.right:
             singleChildRule()
                 
                 
 
     def delNode(self, newNode): #recursive remove function. There's three rules to this though
         #Rule one: if it's got no children we just delete it's shit.
-        if not left and not right:
+        if not self.left and not self.right:
             noChildRule()
             return
         #Rule two: one child case: We move the pointer around then delete the node, we need the parent for this.
